805c72812915abbbcd92a7e196e25b828092a528
schofieldlabs <ralph.schofield@gmail.com>
2025-06-13 16:20:43 -0500

    neverending

--- run.py
+++ run.py
@@ -81,7 +81,10 @@ def create_app():
 
     return app
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app = create_app()
     app.run(host='0.0.0.0', port=port)
+
+# This makes it work for Render:
+app = create_app()
