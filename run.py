9061fdd3fd886ab312bc8eea9d7638ceb8af6cae
schofieldlabs <ralph.schofield@gmail.com>
2025-06-13 14:01:44 -0500

    port fix attempt

--- run.py
+++ run.py
@@ -70,6 +70,6 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app = create_app()
     app.run(host='0.0.0.0', port=port)
