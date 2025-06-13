3387816f910dcee7f9b02d01957223477cbc143f
schofieldlabs <ralph.schofield@gmail.com>
2025-06-13 13:41:22 -0500

    port update

--- run.py
+++ run.py
@@ -70,5 +70,6 @@ def create_app():
     return app
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
